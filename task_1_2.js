83af5157dc045ceff2ceb251d503223b3abac30c
Sergey Lubimov <siarhei_liubimau@epam.com>
2022-12-18 12:54:31 +0300

    feat: task 1.2

---
+++ task_1_2.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const csv = require('csvtojson')
+
+const readStream = require('fs').createReadStream('./csv/data.csv', {
+    highWaterMark: 2
+})
+ 
+const writeStream =  require('fs').createWriteStream('./csv/data.txt')
+ 
+readStream
+    .on('data', () => { console.log(readStream.read())})
+    .on('error', function(e){ console.log('Error during file reading: ', e)})
+    .pipe(csv())
+    .on('error', function(e){ console.log('Error during CSV parsing: ', e)})
+    .pipe(writeStream)
+    .on('error', function(e){ console.log('Error during writing: ', e)})
