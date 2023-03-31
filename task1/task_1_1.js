21a770952a85a9764ee70324eba28838004d7154
Sergey Lubimov <siarhei_liubimau@epam.com>
2023-03-31 07:57:32 +0300

    task-3: init task-3

--- task1/task_1_1.js
+++
@@ -1,8 +0,0 @@
-function reverseString(str) {
-    return str.split('').reverse().join('');
-}
-
-process.stdin.on("data", data => {
-    data = reverseString(data.toString())
-    process.stdout.write(data + "\n")
-})
