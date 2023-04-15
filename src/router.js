3e8a8f4fa24b4a36ac39fe99d7ef9dc6bc84ffc5
Sergey Lubimov <siarhei_liubimau@epam.com>
2023-04-15 16:21:24 +0300

    fix: open user registration & signin endpoints

--- src/router.js
+++ src/router.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import userRouter from './routes/user.js'
 import groupRouter from './routes/group.js'
+import login from './middleware/jwtauth.js'
 
 const router = express()
 router.use('/users', userRouter)
-router.use('/groups', groupRouter)
+router.use('/groups', login, groupRouter)
 
 export default router
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,14 @@
 import express from 'express'
 const userRouter = express()
 import userController from '../controllers/user.js'
+import login from '../middleware/jwtauth.js'
 
 /* user Router */
-userRouter.get('/', userController.listAllUsers);
-userRouter.get('/:id', userController.getUserById);
+userRouter.get('/', login, userController.listAllUsers);
+userRouter.get('/:id', login, userController.getUserById);
 userRouter.post('/', userController.addUser);
-userRouter.put('/:id', userController.updateUser);
-userRouter.delete('/:id', userController.deleteUser);
+userRouter.put('/:id', login, userController.updateUser);
+userRouter.delete('/:id', login, userController.deleteUser);
 userRouter.post('/signin', userController.signin);
 
 export default userRouter
